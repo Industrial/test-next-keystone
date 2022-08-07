b2e4dbb58b89c6bca6622456b337318a61c329c0
Tom Wieland <tom.wieland@gmail.com>
2022-08-07 15:51:54 +0200

    feature(api): implement keystone cms

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const { withKeystone } = require("@keystone-6/core/next");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withKeystone(nextConfig)
