c218fb31da3da3b49692c9441d03f3e395e4875c
Tom Wieland <tom.wieland@gmail.com>
2022-08-12 16:38:21 +0200

    feature(api): use bcrypt for passwords

--- models/User.ts
+++ models/User.ts
@@ -1,5 +1,6 @@
 import { list } from '@keystone-6/core'
 import { checkbox, password, text } from '@keystone-6/core/fields'
+import bcrypt from 'bcryptjs'
 
 import {
   isAdminAccessOperation,
@@ -12,11 +13,31 @@ import { Lists } from '.keystone/types'
 export const User: Lists.User = list({
   fields: {
     username: text({
+      label: 'Username',
       isIndexed: 'unique',
+      isFilterable: true,
+      isOrderable: true,
+      validation: {
+        isRequired: true,
+        length: {
+          min: 3,
+          max: 36,
+        },
+      },
     }),
 
     email: text({
+      label: 'Email',
       isIndexed: 'unique',
+      isFilterable: true,
+      isOrderable: true,
+      validation: {
+        isRequired: true,
+        length: {
+          min: 3,
+          max: 254, // http://stackoverflow.com/a/574698/1945631
+        },
+      },
       access: {
         read: isAdminOrCurrentUserAccessOperation,
         update: isCurrentUserAccessOperation,
@@ -24,13 +45,27 @@ export const User: Lists.User = list({
     }),
 
     password: password({
+      label: 'Password',
+      isFilterable: true,
+      isOrderable: true,
+      validation: {
+        isRequired: true,
+        length: {
+          min: 3,
+          // max: 4096,
+        },
+      },
       access: {
         read: isAdminOrCurrentUserAccessOperation,
         update: isCurrentUserAccessOperation,
       },
+      bcrypt,
     }),
 
     isAdmin: checkbox({
+      label: 'Admin',
+      isFilterable: true,
+      isOrderable: true,
       access: {
         read: isUserAccessOperation,
         update: isAdminAccessOperation,
